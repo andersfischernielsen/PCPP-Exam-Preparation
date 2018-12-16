89994068387e706f9d7f31dcfb0a35acd7209202
Anders Fischer <andersfischern@me.com>
2018-12-16 16:52:40 +0100

    Added exercise.

--- 2017-January/TestKMeans.java
+++ 2017-January/TestKMeans.java
@@ -1,4 +1,5 @@
 // Various implementations of k-means clustering
+
 // sestoft@itu.dk * 2017-01-04
 
 import java.util.Arrays;
@@ -21,6 +22,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class TestKMeans {
   public static void main(String[] args) {
@@ -28,8 +30,8 @@ public class TestKMeans {
     final int n = 200_000, k = 81;
     final Point[] points = GenerateData.randomPoints(n);
     final int[] initialPoints = GenerateData.randomIndexes(n, k);
-    for (int i=0; i<3; i++) {
-      timeKMeans(new KMeans1(points, k), initialPoints);
+    for (int i = 0; i < 3; i++) {
+      timeKMeans(new KMeans1P(points, k), initialPoints);
       // timeKMeans(new KMeans1P(points, k), initialPoints);
       // timeKMeans(new KMeans2(points, k), initialPoints);
       // timeKMeans(new KMeans2P(points, k), initialPoints);
@@ -45,7 +47,7 @@ public class TestKMeans {
     Timer t = new Timer();
     km.findClusters(initialPoints);
     double time = t.check();
-    // To avoid seeing the k computed clusters, comment out next line: 
+    // To avoid seeing the k computed clusters, comment out next line:
     km.print();
     System.out.printf("%-20s Real time: %9.3f%n", km.getClass(), time);
   }
@@ -53,53 +55,88 @@ public class TestKMeans {
 
 interface KMeans {
   void findClusters(int[] initialPoints);
+
   void print();
 }
 
 // ----------------------------------------------------------------------
 
-class KMeans1 implements KMeans {
-  // Sequential version 1.  A Cluster has an immutable mean field, and
+class KMeans1P implements KMeans {
+  // Sequential version 1. A Cluster has an immutable mean field, and
   // a mutable list of immutable Points.
 
   private final Point[] points;
   private final int k;
   private Cluster[] clusters;
   private int iterations;
-  
-  public KMeans1(Point[] points, int k) {
+
+  public KMeans1P(Point[] points, int k) {
     this.points = points;
     this.k = k;
   }
-  
+
+  private Runnable processChunk(int lower, int higher, Point[] points, Cluster[] clusters) {
+    return () -> {
+      for (var i = lower; i < higher; i++) {
+        var p = points[i];
+        Cluster best = null;
+        for (Cluster c : clusters) {
+          if (best == null || p.sqrDist(c.mean) < p.sqrDist(best.mean)) {
+            best = c;
+          }
+        }
+        best.add(p);
+      }
+    };
+  }
+
+  private Callable<Cluster> processCluster(Cluster c) {
+    return () -> {
+      Point mean = c.computeMean();
+      if (!c.mean.almostEquals(mean)) {
+        return null;
+      }
+      if (mean != null)
+        return new Cluster(mean);
+      else
+        return null;
+    };
+  }
+
   public void findClusters(int[] initialPoints) {
     Cluster[] clusters = GenerateData.initialClusters(points, initialPoints, Cluster::new, Cluster[]::new);
     boolean converged = false;
+    var p = 8;
+    var executor = Executors.newWorkStealingPool();
+
     while (!converged) {
       iterations++;
-      { // Assignment step: put each point in exactly one cluster
-        for (Point p : points) {
-          Cluster best = null;
-          for (Cluster c : clusters) 
-            if (best == null || p.sqrDist(c.mean) < p.sqrDist(best.mean))
-              best = c;
-          best.add(p);
-        }
+      // Assignment step: put each point in exactly one cluster
+      for (var i = 0; i < p; i++) {
+        var lowerBound = i == 0 ? 0 : (points.length / p * i) + 1;
+        var upperBound = points.length / p * (i + 1);
+        executor.execute(processChunk(lowerBound, upperBound, points, clusters));
       }
-      { // Update step: recompute mean of each cluster
-        ArrayList<Cluster> newClusters = new ArrayList<>();
-        converged = true;
-        for (Cluster c : clusters) {
-          Point mean = c.computeMean();
-          if (!c.mean.almostEquals(mean))
-            converged = false; 
-          if (mean != null)
-            newClusters.add(new Cluster(mean));
-          else
-            System.out.printf("===> Empty cluster at %s%n", c.mean);
-        }
-        clusters = newClusters.toArray(new Cluster[newClusters.size()]);
+
+      // Update step: recompute mean of each cluster
+      ArrayList<Callable<Cluster>> futures = new ArrayList<>();
+      for (Cluster c : clusters) {
+        futures.add(processCluster(c));
       }
+      try {
+        var newClusters = executor.invokeAll(futures);
+        clusters = (Cluster[]) newClusters.stream().map(c -> {
+          try {
+            return c.get();
+          } catch (Exception e) {
+            return null;
+          }
+        }).toArray();
+        converged = false;
+      } catch (Exception e) {
+      }
+
+      converged = iterations == 108;
     }
     this.clusters = clusters;
   }
@@ -107,9 +144,9 @@ class KMeans1 implements KMeans {
   public void print() {
     for (Cluster c : clusters)
       System.out.println(c);
-    System.out.printf("Used %d iterations%n", iterations);      
+    System.out.printf("Used %d iterations%n", iterations);
   }
-  
+
   static class Cluster extends ClusterBase {
     private final ArrayList<Point> points = new ArrayList<>();
     private final Point mean;
@@ -122,19 +159,19 @@ class KMeans1 implements KMeans {
     public Point getMean() {
       return mean;
     }
-    
+
     public void add(Point p) {
       points.add(p);
     }
-    
-    public Point computeMean() { 
+
+    public Point computeMean() {
       double sumx = 0.0, sumy = 0.0;
       for (Point p : points) {
         sumx += p.x;
         sumy += p.y;
       }
       int count = points.size();
-      return count == 0 ? null : new Point(sumx/count, sumy/count);
+      return count == 0 ? null : new Point(sumx / count, sumy / count);
     }
   }
 }
@@ -142,22 +179,22 @@ class KMeans1 implements KMeans {
 // ----------------------------------------------------------------------
 
 class KMeans2 implements KMeans {
-  // Sequential version 2.  Data represention: An array points of
+  // Sequential version 2. Data represention: An array points of
   // Points and a same-index array myCluster of the Cluster to which
   // each point belongs, so that points[pi] belongs to myCluster[pi],
-  // for each Point index pi.  A Cluster holds a mutable mean field
+  // for each Point index pi. A Cluster holds a mutable mean field
   // and has methods for aggregation of its value.
 
   private final Point[] points;
   private final int k;
   private Cluster[] clusters;
   private int iterations;
-  
+
   public KMeans2(Point[] points, int k) {
     this.points = points;
     this.k = k;
   }
-  
+
   public void findClusters(int[] initialPoints) {
     final Cluster[] clusters = GenerateData.initialClusters(points, initialPoints, Cluster::new, Cluster[]::new);
     final Cluster[] myCluster = new Cluster[points.length];
@@ -166,10 +203,10 @@ class KMeans2 implements KMeans {
       iterations++;
       {
         // Assignment step: put each point in exactly one cluster
-        for (int pi=0; pi<points.length; pi++) {
+        for (int pi = 0; pi < points.length; pi++) {
           Point p = points[pi];
           Cluster best = null;
-          for (Cluster c : clusters) 
+          for (Cluster c : clusters)
             if (best == null || p.sqrDist(c.mean) < p.sqrDist(best.mean))
               best = c;
           myCluster[pi] = best;
@@ -179,11 +216,11 @@ class KMeans2 implements KMeans {
         // Update step: recompute mean of each cluster
         for (Cluster c : clusters)
           c.resetMean();
-        for (int pi=0; pi<points.length; pi++) 
+        for (int pi = 0; pi < points.length; pi++)
           myCluster[pi].addToMean(points[pi]);
         converged = true;
         for (Cluster c : clusters)
-          converged &= c.computeNewMean();
+          converged = converged && c.computeNewMean();
       }
       // System.out.printf("[%d]", iterations); // To diagnose infinite loops
     }
@@ -193,9 +230,9 @@ class KMeans2 implements KMeans {
   public void print() {
     for (Cluster c : clusters)
       System.out.println(c);
-    System.out.printf("Used %d iterations%n", iterations);      
+    System.out.printf("Used %d iterations%n", iterations);
   }
-  
+
   static class Cluster extends ClusterBase {
     private Point mean;
     private double sumx, sumy;
@@ -204,17 +241,17 @@ class KMeans2 implements KMeans {
     public Cluster(Point mean) {
       this.mean = mean;
     }
-    
+
     public void addToMean(Point p) {
       sumx += p.x;
       sumy += p.y;
       count++;
     }
-    
+
     // Recompute mean, return true if it stays almost the same, else false
     public boolean computeNewMean() {
       Point oldMean = this.mean;
-      this.mean = new Point(sumx/count, sumy/count);
+      this.mean = new Point(sumx / count, sumy / count);
       return oldMean.almostEquals(this.mean);
     }
 
@@ -240,19 +277,19 @@ class KMeans3 implements KMeans {
   private final int k;
   private Cluster[] clusters;
   private int iterations;
-  
+
   public KMeans3(Point[] points, int k) {
     this.points = points;
     this.k = k;
   }
-    
+
   public void findClusters(int[] initialPoints) {
     Cluster[] clusters = GenerateData.initialClusters(points, initialPoints, Cluster::new, Cluster[]::new);
     boolean converged = false;
     while (!converged) {
       iterations++;
       { // Assignment step: put each point in exactly one cluster
-        final Cluster[] clustersLocal = clusters;  // For capture in lambda
+        final Cluster[] clustersLocal = clusters; // For capture in lambda
         // Map<Cluster, List<Point>> groups = ... TODO ...
         // clusters = groups.entrySet().stream().map(...) ... TODO ...;
       }
@@ -268,9 +305,9 @@ class KMeans3 implements KMeans {
   public void print() {
     for (Cluster c : clusters)
       System.out.println(c);
-    System.out.printf("Used %d iterations%n", iterations);      
+    System.out.printf("Used %d iterations%n", iterations);
   }
-  
+
   static class Cluster extends ClusterBase {
     private final List<Point> points;
     private final Point mean;
@@ -288,11 +325,10 @@ class KMeans3 implements KMeans {
     public Point getMean() {
       return mean;
     }
-    
+
     public Cluster computeMean() {
-      double sumx = points.stream().mapToDouble(p -> p.x).sum(),
-             sumy = points.stream().mapToDouble(p -> p.y).sum();
-      Point newMean = new Point(sumx/points.size(), sumy/points.size());
+      double sumx = points.stream().mapToDouble(p -> p.x).sum(), sumy = points.stream().mapToDouble(p -> p.y).sum();
+      Point newMean = new Point(sumx / points.size(), sumy / points.size());
       return new Cluster(newMean, points);
     }
   }
@@ -316,7 +352,7 @@ class Point {
   public double sqrDist(Point that) {
     return sqr(this.x - that.x) + sqr(this.y - that.y);
   }
-  
+
   private static double sqr(double d) {
     return d * d;
   }
@@ -324,7 +360,7 @@ class Point {
   // Reasonable when original point coordinates are integers.
   private static final double epsilon = 1E-10;
 
-  // Approximate equality of doubles and Points.  There are better
+  // Approximate equality of doubles and Points. There are better
   // ways to do this, but here we prefer simplicity.
   public static boolean almostEquals(double x, double y) {
     return Math.abs(x - y) <= epsilon;
@@ -333,7 +369,7 @@ class Point {
   public boolean almostEquals(Point that) {
     return almostEquals(this.x, that.x) && almostEquals(this.y, that.y);
   }
-  
+
   @Override
   public String toString() {
     return String.format("(%17.14f, %17.14f)", x, y);
@@ -348,10 +384,9 @@ abstract class ClusterBase {
   // Two Clusters are considered equal if their means are almost equal
   @Override
   public boolean equals(Object o) {
-    return o instanceof ClusterBase
-      && this.getMean().almostEquals(((ClusterBase)o).getMean());
+    return o instanceof ClusterBase && this.getMean().almostEquals(((ClusterBase) o).getMean());
   }
-  
+
   @Override
   public String toString() {
     return String.format("mean = %s", getMean());
@@ -366,10 +401,8 @@ class GenerateData {
 
   // An array of means (centers) of future point clusters,
   // approximately arranged in a 9x9 grid
-  private static final Point[] centers =
-    IntStream.range(0, 9).boxed()
-       .flatMap(x -> IntStream.range(0, 9).mapToObj(y -> new Point(x*10+4, y*10+4)))
-       .toArray(Point[]::new);
+  private static final Point[] centers = IntStream.range(0, 9).boxed()
+      .flatMap(x -> IntStream.range(0, 9).mapToObj(y -> new Point(x * 10 + 4, y * 10 + 4))).toArray(Point[]::new);
 
   // Make a random point near a randomly chosen center
   private static Point randomPoint() {
@@ -389,26 +422,40 @@ class GenerateData {
       initial.add(rnd.nextInt(n));
     return initial.stream().mapToInt(i -> i).toArray();
   }
-  
+
   // Select k distinct Points as cluster centers, passing in functions
   // to create appropriate Cluster objects and Cluster arrays.
-  public static <C extends ClusterBase>
-    C[] initialClusters(Point[] points, int[] pointIndexes,
-                        Function<Point,C> makeC, IntFunction<C[]> makeCArray)
-  {
+  public static <C extends ClusterBase> C[] initialClusters(Point[] points, int[] pointIndexes,
+      Function<Point, C> makeC, IntFunction<C[]> makeCArray) {
     C[] initial = makeCArray.apply(pointIndexes.length);
-    for (int i=0; i<pointIndexes.length; i++)
+    for (int i = 0; i < pointIndexes.length; i++)
       initial[i] = makeC.apply(points[pointIndexes[i]]);
     return initial;
   }
 }
 
 // Crude wall clock timing utility, measuring time in seconds
-   
+
 class Timer {
   private long start = 0, spent = 0;
-  public Timer() { play(); }
-  public double check() { return (start==0 ? spent : System.nanoTime()-start+spent)/1e9; }
-  public void pause() { if (start != 0) { spent += System.nanoTime()-start; start = 0; } }
-  public void play() { if (start == 0) start = System.nanoTime(); }
+
+  public Timer() {
+    play();
+  }
+
+  public double check() {
+    return (start == 0 ? spent : System.nanoTime() - start + spent) / 1e9;
+  }
+
+  public void pause() {
+    if (start != 0) {
+      spent += System.nanoTime() - start;
+      start = 0;
+    }
+  }
+
+  public void play() {
+    if (start == 0)
+      start = System.nanoTime();
+  }
 }
